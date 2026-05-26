624d7e2a12822a1c6125d72ba3c9bd504cd88bde
Isaac Gabriel <isaacgbr7@gmail.com>
2026-05-25 23:12:23 -0300

    corrige controller de hospede

--- src/controllers/hospede.controller.js
+++ src/controllers/hospede.controller.js
@@ -1,18 +1,18 @@
 const service = require('../services/hospede.service');
 
 const criar = async (req, res) => {
-    const hospede = await service.criar(req.body);
+    const hospede = await service.criarHospede(req.body);
     res.status(201).json(hospede);
 };
 
 const listar = async (req, res) => {
-    const hospedes = await service.listar();
+    const hospedes = await service.listarHospedes();
     res.json(hospedes);
 };
 
 const buscar = async (req, res) => {
     const id = parseInt(req.params.id);
-    const hospede = await service.buscar(id);
+    const hospede = await service.buscarHospedePorId(id);
 
     if (!hospede) {
         return res.status(404).json({ erro: "Não encontrado" });
@@ -23,13 +23,13 @@ const buscar = async (req, res) => {
 
 const atualizar = async (req, res) => {
     const id = parseInt(req.params.id);
-    const hospede = await service.atualizar(id, req.body);
+    const hospede = await service.atualizarHospede(id, req.body);
     res.json(hospede);
 };
 
 const deletar = async (req, res) => {
     const id = parseInt(req.params.id);
-    await service.deletar(id);
+    await service.deletarHospede(id);
     res.json({ mensagem: "Deletado com sucesso" });
 };
 
